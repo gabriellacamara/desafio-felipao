cdcf1b38adb4e76cc327e2ea0c0eb2a3219edda9
Gabriella Camara <agabriellacamara@gmail.com>
2026-02-10 21:19:18 -0300

    update1

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
 let champion = {
     nome: "Diana",
-    xp: 6200,
+    xp: 8200,
 }
 
 let nivel = ""; 
 
 switch (true) { 
-    case champion.xp < 1000:
-        nivel = `O nível de ${champion.nome} é: Ferro`
-    break
+    case champion.xp <= 1000:
+        nivel = "Ferro";
+        break;
 
-    case champion.xp >= 1001 && champion.xp <= 2000: 
-        nivel = `O nível de ${champion.nome} é: Bronze`
-    break
+    case champion.xp <= 2000: 
+        nivel = "Bronze";
+        break;
 
-    case champion.xp >= 2001 && champion.xp <= 3000: 
-        nivel = `O nível de ${champion.nome} é: Prata Ouro`
-    break
+    case champion.xp <= 3000: 
+        nivel = "Prata";
+        break;
 
-   case champion.xp >= 5001 && champion.xp <= 8000:
-        nivel = `O nível de ${champion.nome} é: Platina Diamante`
-   break
+    case champion.xp <= 4000:
+        nivel = "Ouro";
+        break;
 
-    case champion.xp >= 8001 && champion.xp <= 9000:
-        nivel = `O nível de ${champion.nome} é: Ascendente`
-    break
+    case champion.xp <= 5000:
+        nivel = "Ouro"; 
+        break;
+    
+    case champion.xp <= 6000:
+        nivel = "Platina";
+        break;
 
-    default:
-        nivel = `O nível de ${champion.nome} é: Suprassumo`
+    case champion.xp <= 7000:
+        nivel = "Diamante";
+        break;
 
+    case champion.xp <= 8000:
+        nivel = "Ascendente";
+        break;
+
+    case champion.xp <= 10000:
+        nivel = "Radiante";
+        break;
+
+    case champion.xp > 10000:
+        nivel = "Imortal";
+        break;
 }
 
-console.log(nivel)
+console.log(`O campeão ${champion.nome} está no nível ${nivel}`);
